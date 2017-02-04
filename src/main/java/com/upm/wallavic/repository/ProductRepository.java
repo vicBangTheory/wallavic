938539ab167a3fc5b2fcc96f5a9757aa2c3728df
Victor <victor.bussines.mail@gmail.com>
2017-02-04 11:36:40 +0100

    gbcf

--- src/main/java/com/upm/wallavic/repository/ProductRepository.java
+++ src/main/java/com/upm/wallavic/repository/ProductRepository.java
@@ -18,10 +18,10 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     @Query("select product from Product product where product.user.login = ?#{principal.username}")
     List<Product> findByUserIsCurrentUser();
 
-    @Query("select p from Product p where p.price <= ?1 and p.price >= ?2 and p.sold = false and product.user.login <> ?#{principal.username}")
+    @Query("select p from Product p where p.price <= ?1 and p.price >= ?2 and p.sold = false and p.user.login <> ?#{principal.username}")
     Page<Product> finAllByPrice(Pageable pageable, Double maxPrice, Double minPrice);
 
-    @Query("select p from Product p where p.price <= ?1 and p.price >= ?2 and p.cat in ?3 and product.user.login <> ?#{principal.username}")
+    @Query("select p from Product p where p.price <= ?1 and p.price >= ?2 and p.cat in ?3 and p.user.login <> ?#{principal.username}")
     Page<Product> finAllByPriceAndCat(Double maxPrice, Double minPrice, List<ProductCat> cats, Pageable pageable);
 
 
